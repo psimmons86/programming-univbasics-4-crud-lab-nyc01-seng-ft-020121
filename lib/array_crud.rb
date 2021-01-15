0d573cd16765bf82223b92ecb076d61d4e1dd24e
patrick simmons <hadroncollides@gmail.com>
2021-01-15 17:59:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,9 +24,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[1]
-  
-end
+  array[0]
+  end
 
 def retrieve_first_element_from_array(array)
   
