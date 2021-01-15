2157631d660780d78e0e291bd3290442b48dd561
patrick simmons <hadroncollides@gmail.com>
2021-01-15 17:51:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
   p radiohead
 end
 
-def add_element_to_end_of_array(radiohead,"colin")
-radiohead >> "colin"
+def add_element_to_end_of_array(array, element)
+array >> element
 end
 
 def add_element_to_start_of_array(array, element)
